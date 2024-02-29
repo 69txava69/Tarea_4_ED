a6a8bc6b59abb4e103c5dfa6fdaab2b1b7c9164f
Diego <txava.diego@gmail.com>
2024-02-29 19:04:29 +0100

    clase persona con contructor y gettrs & setters

--- src/jubilacion/Persona.java
+++ src/jubilacion/Persona.java
@@ -1,7 +1,52 @@
 package jubilacion;
 
-public class Persona {
-	
-	
+import java.util.Scanner;
+
+/**
+	 * clase llamada Persona con un atributo llamado edad
+	 * 
+	 * @author Diego Bello
+	 */
+
+	public class Persona {
+
+		private int edad = 0;
+
+		/**
+		 * <strong>constructor Método constructor para el ojeto persona con el parámetro
+		 * edad
+		 * 
+		 * @param edad. Debe ser un parámetro de tipo entero
+		 */
+		public Persona(int edad) {
+			super();
+			this.edad = edad;
+		}
+
+		/**
+		 * Método Get para que nos muestre la edad
+		 * 
+		 * @return La edad
+		 */
+		public int getEdad() {
+			return edad;
+		}
+		
+		/**
+		 * Método set para dar valor a la edad La edad es un tipo de dato entero que es
+		 * pedido por consola
+		 * 
+		 * 
+		 * @param edad the edad to set
+		 * 
+		 * @see Clase Scanner del API de Java
+		 */
+		public void setEdad(int edad) {
+			System.out.println("Introduce tu edad");
+			@SuppressWarnings("resource")
+			Scanner teclado = new Scanner(System.in);
+
+			this.edad = teclado.nextInt();
+		}
 
 }
